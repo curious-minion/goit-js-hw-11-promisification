6a4544da261e3956e9e49752806e19bd12fde746
courios-minion <okarskaolya@gmail.com>
2021-05-27 14:18:00 +0200

    tasks 2 & 3 solved

--- task-3.js
+++ task-3.js
@@ -1,41 +1,32 @@
-// Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
-
-  setTimeout(() => {
+  return new Promise((resolve, reject) =>
+  {
+    setTimeout(() => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      onSuccess(transaction.id, delay);
+      resolve([transaction.id, delay]);
     } else {
-      onError(transaction.id);
+      reject(transaction.id);
     }
-  }, delay);
+}, delay);
+})
+ 
 };
 
-const logSuccess = (id, time) => {
+function logSuccess([id, time]) {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = id => {
+function logError(id) {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
